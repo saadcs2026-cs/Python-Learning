4abdf8a10a20080d5c9b9f1ab0151705f5559220
Muhammad Saad <saad.cs.2026@gmail.com>
2026-04-29 23:08:12 +0500

    Sum of numbers using for loop

---
+++ Sum of 5 numbers using for loop.py
@@ -0,0 +1,23 @@
+total = 0
+
+for x in range(5):
+    
+    # take input here
+    number = int(input("Enter number:  "))
+    
+    # add it to total
+    total += number
+    
+# print total here
+print("The sum of numbers is: ",total)
+
+
+
+    
+    
+    
+
+
+
+    
+
